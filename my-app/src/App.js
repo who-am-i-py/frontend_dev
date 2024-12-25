bbc4ae52a9e00abaf82ad02977b55007d8857c06
who-am-i-py <sys.whoami.py@gmail.com>
2024-12-25 23:56:09 +0100

    Import bootstrap.css in my SPA

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import "./bootstrap.css";
 function App() {
   return (
     <div className="App">
