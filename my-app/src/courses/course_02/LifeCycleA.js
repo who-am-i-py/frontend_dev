de227338ee0f590ff46a3ad4ef061cff241a79e2
who-am-i-py <sys.whoami.py@gmail.com>
2024-12-28 16:35:39 +0100

    Here I finish up the update phase LifeCycle

--- my-app/src/courses/course_02/LifeCycleA.js
+++ my-app/src/courses/course_02/LifeCycleA.js
@@ -16,10 +16,33 @@ class LifeCycleA extends Component {
   componentDidMount = () => {
     console.log("[componentDidMount]==[LifeCycleA]");
   };
+  shouldComponentUpdate = () => {
+    console.log("[shouldComponentUpdate]==[LifeCycleA]");
+    return true;
+  };
+  getSnapshotBeforeUpdate = (prevState, prevProp) => {
+    console.log("[getSnapshotBeforeUpdate]==[LifeCycleA]");
+    return null;
+  };
+  componentDidUpdate = () => {
+    console.log("[componentDidUpdate]==[LifeCycleA]");
+  };
+  updateHandler = () => {
+    console.log("[updateHandler]==[LifeCycleA]");
+    this.setState({ first: "Goodbye " });
+  };
+
   render() {
     console.log("[render]==[LifeCycleA]");
 
-    return <div>LifeCycleA</div>;
+    return (
+      <div>
+        <h1>LifeCycleA, {this.state.first}</h1>
+        <button className="btn btn-primary" onClick={this.updateHandler}>
+          Update Phase
+        </button>
+      </div>
+    );
   }
 }
 
