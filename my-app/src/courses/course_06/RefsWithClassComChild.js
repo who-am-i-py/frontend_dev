01ac26306aa54569be8931375dedeeb5786d2bb2
who-am-i-py <sys.whoami.py@gmail.com>
2025-01-01 23:57:25 +0100

    Here we work with pass refs with class component, at this time I build my child component with its ref Child=>Parent pass

---
+++ my-app/src/courses/course_06/RefsWithClassComChild.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class RefsWithClassComChild extends Component {
+  constructor(props) {
+    super(props);
+    this.inputRef = React.createRef();
+  }
+  inputFocus = () => {
+    this.inputRef.current.focus();
+  };
+  render() {
+    return (
+      <div>
+        <h1>Ref with class component</h1>
+        <input ref={this.inputRef} />
+      </div>
+    );
+  }
+}
+
+export default RefsWithClassComChild;
